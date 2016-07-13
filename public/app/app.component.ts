f8e6fdc9b003e8c79d3d4b6ca0ba8b298fe34613
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-07-13 14:54:16 +0300

    initial commit

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { UsersComponent } from '../users/users.component';
 
 
 @Component({
     selector: 'app',
     templateUrl: '/public/app/app.component.html',
-    directives: [UsersComponent]
 })
 export class AppComponent { }
--- public/main.ts
+++ public/main.ts
@@ -1,11 +1,9 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { UsersService } from './users/users.service';
 import 'rxjs/add/operator/toPromise';
 
 import { AppComponent } from './app/app.component';
 
 bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  UsersService
+  HTTP_PROVIDERS
 ]);
